35b5b0672b77786085f6ceb25db2f0ca8f388be0
Pritish Raj <pritishraj.official@gmail.com>
2025-12-08 22:46:10 +0530

    First commit

---
+++ src/main/java/com/codingShuttle/collegeMangementSystem/collegeManagementSystem/CollegeManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.codingShuttle.collegeMangementSystem.collegeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementSystemApplication.class, args);
+        //This will have all the changes related to college management system project
+        // Repo name : https://github.com/praj031/collegeManagementSystem.git
+	}
+
+}
